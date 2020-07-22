a51fea47d61ec2f87ab72cdad7f8cfd2cb28ff4a
Jonathan Mah <jon@jonmah.com>
2020-07-22 01:41:35 -0700

    Added theme toggling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled, { ThemeProvider } from 'styled-components'
 
+import { GlobalStyles } from './assets/global'
+import { dark, light } from './assets/themes'
 import Switch from './components/Switch'
 
 const Container = styled.div`
-  background: ${props => props.theme.dark.background};
+  background: ${props => props.background};
   height: 100%;
   width: 100%;
 `
 Container.displayName = 'AppContainer'
 
 const App = () => {
+  const [theme, setTheme] = useState('light')
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }
   return (
-    <ThemeProvider
-      theme={{
-        dark: { background: '#29416f' },
-        light: { background: '#ffffff' },
-      }}
-    >
+    <ThemeProvider theme={theme === 'dark' ? dark : light}>
+      <GlobalStyles />
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Container>
         <Switch handleToggle={() => console.log('flip')} />
       </Container>
