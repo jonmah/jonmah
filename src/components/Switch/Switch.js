402ac7cb6dd11758605cd8dab740684e597567b4
Jonathan Mah <jon@jonmah.com>
2020-07-31 22:01:16 -0700

    Update UX for Switch

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,58 +1,7 @@
 import React from 'react'
 import uuid from 'react-uuid'
-import styled from 'styled-components'
 
-const Input = styled.input`
-  height: 0;
-  visibility: hidden;
-  width: 0;
-
-  &:checked + label {
-    background: ${({ theme }) => theme.colors.blueNight};
-  }
-
-  &:checked + label span {
-    left: calc(100% - 7px);
-    transform: translateX(-100%);
-  }
-
-  &:checked + label span {
-    background: ${({ theme }) => theme.colors.greyMoon};
-  }
-`
-Input.displayName = 'SwitchInput'
-
-const Label = styled.label`
-  &:active span {
-    width: 60px;
-  }
-
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  cursor: pointer;
-  width: 100px;
-  height: 50px;
-  background: ${({ theme }) => theme.colors.blueLight};
-  border-radius: 100px;
-  position: relative;
-  transition: background-color 0.2s;
-`
-Label.displayName = 'SwitchLabel'
-
-const Button = styled.span`
-  content: '';
-  position: absolute;
-  top: 7x;
-  left: 7px;
-  width: 36px;
-  height: 36px;
-  border-radius: 36px;
-  transition: 0.2s;
-  background: ${({ theme }) => theme.colors.yellowSun};
-  box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.29);
-`
-Button.displayName = 'SwitchButton'
+import { Button, Input, Label } from './styles'
 
 const Switch = ({ handleToggle, isOn }) => {
   const id = uuid()
---
+++ src/components/Switch/styles.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+export const Input = styled.input`
+  height: 0;
+  visibility: hidden;
+  width: 0;
+
+  &:checked + label {
+    background: ${({ theme }) => theme.colors.green};
+  }
+
+  &:checked + label span {
+    left: calc(100% - 2px);
+    transform: translateX(-100%);
+  }
+`
+Input.displayName = 'SwitchInput'
+
+export const Label = styled.label`
+  &:active span {
+    width: 45px;
+  }
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+  width: 60px;
+  height: 36px;
+  background: ${({ theme }) => theme.colors.orange};
+  border-radius: 80px;
+  position: relative;
+  transition: background-color 0.2s;
+`
+Label.displayName = 'SwitchLabel'
+
+export const Button = styled.span`
+  content: '';
+  position: absolute;
+  top: 2x;
+  left: 2px;
+  width: 32px;
+  height: 32px;
+  border-radius: 32px;
+  transition: 0.2s;
+  background: ${({ theme }) => theme.colors.white};
+  box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.29);
+`
+Button.displayName = 'SwitchButton'
