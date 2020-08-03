c4c33980db191d8960a1bf13e092daa32e79f4d8
Jonathan Mah <jon@jonmah.com>
2020-08-02 18:46:07 -0700

    Convert to PX to REM

--- src/components/Switch/styled.js
+++ src/components/Switch/styled.js
@@ -8,7 +8,7 @@ export const Input = styled.input`
   }
 
   &:checked + label span {
-    left: calc(100% - 2px);
+    left: calc(100% - 0.1rem);
     transform: translateX(-100%);
   }
 `
@@ -16,17 +16,17 @@ Input.displayName = 'SwitchInput'
 
 export const Label = styled.label`
   &:active span {
-    width: 45px;
+    width: 3rem;
   }
 
   display: flex;
   align-items: center;
   justify-content: space-between;
   cursor: pointer;
-  width: 60px;
-  height: 36px;
+  width: 4rem;
+  height: 2.2rem;
   background: ${({ theme }) => theme.colors.grey};
-  border-radius: 80px;
+  border-radius: 2rem;
   position: relative;
   transition: background-color 0.2s;
 `
@@ -35,13 +35,13 @@ Label.displayName = 'SwitchLabel'
 export const Button = styled.span`
   content: '';
   position: absolute;
-  top: 2x;
-  left: 2px;
-  width: 32px;
-  height: 32px;
-  border-radius: 32px;
+  top: 0.1rem;
+  left: 0.1rem;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 2rem;
   transition: 0.2s;
   background: ${({ theme }) => theme.colors.white};
-  box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.29);
+  box-shadow: 0 0 0.2rem 0 rgba(10, 10, 10, 0.29);
 `
 Button.displayName = 'SwitchButton'
