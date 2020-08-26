c0e530a3a9ccdd603fa14fcfceabfff0383d0e18
Jonathan Mah <jon@jonmah.com>
2020-08-25 23:00:10 -0700

    Remove unwanted library

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import themes from './theme'
 import { GlobalStyles } from './theme/global'
 import Header from './Header'
 import { Page } from './components'
-import { capitalize } from 'webpack-js-library'
 
 const App = () => {
   const [theme, setTheme] = useState('light')
@@ -17,7 +16,6 @@ const App = () => {
     }
   }
 
-  console.log(capitalize('sdfsdfsdf'))
   return (
     <ThemeProvider theme={themes[theme]}>
       <GlobalStyles />
