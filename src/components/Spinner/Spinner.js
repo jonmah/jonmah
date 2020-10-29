840503b81382278a3941bba91bf5bdaa83dac0af
Jonathan Mah <jon@jonmah.com>
2020-10-29 01:22:29 -0700

    Added Spinner

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Container } from './styled'
+
+const Spinner = () => (
+  <Container>
+    <div></div>
+    <div></div>
+  </Container>
+)
+
+export default Spinner
