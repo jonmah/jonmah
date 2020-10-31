842214baa198983a4fa1719b57eeb79924acab3c
Jonathan Mah <jon@jonmah.com>
2020-10-31 11:36:10 -0700

    Formatting Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-import {Container} from './styled'
+import { Container } from './styled'
 
 const Card = ({ children, handleClick, handleMouseover, splash }) => {
-  return <Container splash={splash} onClick={handleClick}>{children}</Container>
+  return (
+    <Container splash={splash} onClick={handleClick}>
+      {children}
+    </Container>
+  )
 }
 
 export default Card
