0f72ea595a14086252a3eb993c85fa7ee43fdf33
Jon Mah <jon@jonmah.com>
2025-05-10 17:40:19 -0700

    Theming works

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,24 @@ import { useState } from 'react';
 
 import Toggle from './components/form/Toggle/Toggle';
 import './App.css';
-import './styles/variables.css'
+import './styles/variables.css';
 
 export default function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true)
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
   const handleIsDarkModeToggle = () => {
-    setIsDarkMode(!isDarkMode)
-  }
+    setIsDarkMode(!isDarkMode);
+  };
 
   return (
     <div className="content" data-theme={isDarkMode ? 'dark' : 'light'}>
-      <h1>Rsbuild with React</h1>
-      <Toggle handleToggle={handleIsDarkModeToggle} isOn={isDarkMode} testIdPrefix="App-theme"/>
-      <p>Start building amazing things with Rsbuild.</p>
+      <h1>Jon</h1>
+      <Toggle
+        handleToggle={handleIsDarkModeToggle}
+        isOn={isDarkMode}
+        testIdPrefix="App-theme"
+      />
+      <p>Please listen</p>
     </div>
   );
-};
-
+}
