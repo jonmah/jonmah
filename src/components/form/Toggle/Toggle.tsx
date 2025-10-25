9acc702ecc869886c4101b87bdac844fe36e0837
Jon Mah <jon@jonmah.com>
2025-10-25 02:48:19 -0700

    Theming updates and added Biome

--- src/components/form/Toggle/Toggle.tsx
+++ src/components/form/Toggle/Toggle.tsx
@@ -1,33 +1,46 @@
+import { useId } from 'react';
+
 import styles from './Toggle.module.css';
 
 type ToggleProps = {
-  handleToggle(): void;
-  isOn: boolean;
-  testIdPrefix: string;
-};
+	handleToggle(): void;
+	isDisabled?: boolean;
+	isOn: boolean;
+	label?: string;
+	name?: string;
+	testIdPrefix: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
 export default function Toggle({
-  handleToggle,
-  isOn,
-  testIdPrefix,
-  ...otherProps
+	handleToggle,
+	isDisabled = false,
+	isOn,
+	label = '',
+	name = '',
+	testIdPrefix,
+	...otherProps
 }: ToggleProps) {
-  const id = crypto.randomUUID();
+	const id = useId();
 
-  return (
-    <>
-      <input
-        checked={isOn}
-        className={styles.toggleInput}
-        id={id}
-        onChange={handleToggle}
-        data-testid={`${testIdPrefix}-toggle`}
-        type="checkbox"
-        {...otherProps}
-      />
-      <label htmlFor={id} className={styles.toggleLabel}>
-        <span className={styles.toggleHandler}></span>
-      </label>
-    </>
-  );
+	return (
+		<>
+			<input
+				aria-checked={isOn}
+				aria-label={label}
+				checked={isOn}
+				className={styles.toggleInput}
+				id={id}
+				disabled={isDisabled}
+				name={name}
+				onChange={handleToggle}
+				data-testid={`${testIdPrefix}-toggle`}
+				role="switch"
+				type="checkbox"
+				{...otherProps}
+			/>
+			<label htmlFor={id} className={styles.toggleLabel}>
+				<span className={styles.toggleHandler} />
+			</label>
+		</>
+	);
 }
