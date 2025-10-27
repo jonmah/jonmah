d678064391673494bc296ee104240e90bfd9ecac
Jon Mah <jon@jonmah.com>
2025-10-26 18:01:55 -0700

    Fixed errors in biome configuration

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,21 @@ import './App.css';
 import './styles/variables.css';
 
 export default function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+	const [isDarkMode, setIsDarkMode] = useState(true);
 
-  const handleIsDarkModeToggle = () => {
-    setIsDarkMode(!isDarkMode);
-  };
+	const handleIsDarkModeToggle = () => {
+		setIsDarkMode(!isDarkMode);
+	};
 
-  return (
-    <div className="content" data-theme={isDarkMode ? 'dark' : 'light'}>
-      <h1>Jon</h1>
-      <Toggle
-        handleToggle={handleIsDarkModeToggle}
-        isOn={isDarkMode}
-        testIdPrefix="App-theme"
-      />
-      <p>Please listen</p>
-    </div>
-  );
+	return (
+		<div className="content" data-theme={isDarkMode ? 'dark' : 'light'}>
+			<h1>Jon</h1>
+			<Toggle
+				handleToggle={handleIsDarkModeToggle}
+				isOn={isDarkMode}
+				testIdPrefix="App-theme"
+			/>
+			<p>Please listen</p>
+		</div>
+	);
 }
